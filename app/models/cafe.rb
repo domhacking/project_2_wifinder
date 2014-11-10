7a0c1ac43db4133c7ce98727377a2b908bb1a65e
David Wilkinson <david.wilkinson1984@gmail.com>
2014-11-10 14:34:15 +0000

    added model associations between User, Cafe and CafeUser models.

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -1,3 +1,7 @@
 class Cafe < ActiveRecord::Base
   attr_accessible :building_number, :cafe_description, :cafe_name, :contact_email, :contact_tel, :latitude, :longitude, :opening_hours, :postcode, :street_name, :website_link, :wifi_network_name, :wifi_password
+
+
+  has_many :cafe_users
+  has_many :users, through: :cafe_users
 end
