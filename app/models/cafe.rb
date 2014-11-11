c916e8426f852e9a6fb1457673a2854d6775534b
David Wilkinson <david.wilkinson1984@gmail.com>
2014-11-11 17:55:39 +0000

    can create entry in the join table to show favourite = true in the association, but creates new version each time. needs work

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -1,15 +1,19 @@
 class Cafe < ActiveRecord::Base
-  attr_accessible :building_number, :cafe_description, :cafe_name, :contact_email, :contact_tel, :latitude, :longitude, :opening_hours, :postcode, :street_name, :website_link, :wifi_network_name, :wifi_password
-
+  attr_accessible :building_number, :cafe_description, :cafe_name, :contact_email, :contact_tel, :latitude, :longitude, :opening_hours, :postcode, :street_name, :website_link, :wifi_network_name, :wifi_password, :cafe_users_attributes
 
   has_many :cafe_users
   has_many :users, through: :cafe_users
+  accepts_nested_attributes_for :cafe_users
 
+  ##### GEOCODING
   # auto-fetch coordinates from address
   geocoded_by :postcode
   after_validation :geocode, :if => lambda{ |obj| obj.postcode_changed? }
 
   def full_address
-    
+    # complete this and amend the geocode bit if we want to search by full address rather than just postcode
   end
+  #####
+
+
 end
---
+++ test/unit/helpers/cafes_user_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class CafesUserHelperTest < ActionView::TestCase
+end
