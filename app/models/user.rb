fcebcb3b466a2304f3db7526707c0bab1cb5ce2b
domhacking <dominichacking@gmail.com>
2014-11-11 10:04:33 +0000

    Rmagick and carrierwave css completed and added user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :profile_image
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :profile_image
   # attr_accessible :title, :body
   mount_uploader :profile_image, ProfileImageUploader
 end
