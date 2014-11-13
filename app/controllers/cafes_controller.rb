c9d202edb2af70599286a1fa07ae71518ac61f9a
BethHoulgrave <e.houlgrave@gmail.com>
2014-11-13 22:11:00 +0000

    cafe show list formatted

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -24,9 +24,10 @@ class CafesController < ApplicationController
 
   def index
     @cafes = Cafe.all
-    
-    if current_user.favorite_cafes.any?
-      @faves = Cafe.findFaves(current_user)
+    if current_user
+      if current_user.favorite_cafes.any?
+        @faves = Cafe.findFaves(current_user)
+      end
     end
     # binding.pry
     
